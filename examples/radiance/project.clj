b68c5057dee0b9ef76a14c8e24fe7333cb76630c
Sébastien Le Callonnec <sebastien@weblogism.com>
2024-12-23 16:47:55 +0000

    Migrate example to Radiance L&F.

---
+++ examples/radiance/project.clj
@@ -0,0 +1,6 @@
+(defproject radiance "1.0.0-SNAPSHOT"
+  :description "Example of using Radiance Look and Feel with Seesaw"
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [seesaw "1.5.0"]
+                 [org.pushing-pixels/radiance-theming "7.5.0"]]
+  :main radiance.core)
--- examples/substance/project.clj
+++
@@ -1,7 +0,0 @@
-(defproject substance "1.0.0-SNAPSHOT"
-  :description "Example of using Substance Look and Feel with Seesaw"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]
-                 [com.github.insubstantial/substance "7.1"]]
-  :main substance.core)
-
